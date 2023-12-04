1f0f7d3a9173c3d0c245ec5d65e097c194cecf4b
Pedro Gabriel <pedrpg5@gmail.com>
2023-12-04 13:58:20 -0300

    Melhorias

---
+++ src/br/edu/ifg/ArquivoReserva.java
@@ -0,0 +1,243 @@
+package br.edu.ifg;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ArquivoReserva {
+    private String path;
+
+    public ArquivoReserva(String path) {
+        super();
+        this.path = path;
+    }
+
+    public void cadastrar(String linha) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(this.getPath(), true))) {
+            bw.write(linha);
+            bw.newLine();
+
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao criar o arquivo: " + e.getMessage());
+        }
+    }
+
+    public void buscarTodos(Hotel hotel, int idHotel) {
+        ArrayList<String> linhas = new ArrayList<String>();
+        Map<Integer, Quarto> quartos = new HashMap<>();
+        try {
+
+            String caminho = System.getProperty("user.dir");
+
+            FileReader fileReader = new FileReader(caminho + "/" + this.getPath());
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String linha;
+
+            while ((linha = bufferedReader.readLine()) != null) {
+                String[] dados = linha.split(",");
+                if (Integer.parseInt(dados[6]) == idHotel) {
+                    int id = Integer.parseInt(dados[0]);
+                    String descricao = dados[1];
+                    int qtdCamaCsal = Integer.parseInt(dados[2]);
+                    int qtdCamaSolteiro = Integer.parseInt(dados[3]);
+                    boolean disponivel = Boolean.parseBoolean(dados[4]);
+                    double preco = Double.parseDouble(dados[5]);
+                    Quarto quarto = new Quarto();
+                    quarto.setQuarto(id, preco, descricao, disponivel, qtdCamaCsal, qtdCamaSolteiro, idHotel);
+                    quartos.put(id, quarto);
+                }
+
+            }
+            hotel.setQuartos(quartos);
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao ler o arquivo: " + e.getMessage());
+        }
+    }
+
+    public void buscarQuartosDisponiveis(String cidade, String dataEntrada, String dataSaida) {
+
+    }
+
+    public boolean verificarDisponibilidade(SimpleDateFormat dataIncio, SimpleDateFormat dataFim, Hotel hotel,
+            Quarto quarto) throws ParseException {
+        Date data = new Date();
+        boolean status = true;
+        Date dataInc = dataIncio.parse(dataIncio.format(data));
+        Date dataF = dataFim.parse(dataFim.format(data));
+        try {
+            String caminho = System.getProperty("user.dir");
+
+            FileReader fileReader = new FileReader(caminho + "/" + this.getPath());
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String linha;
+
+            while ((linha = bufferedReader.readLine()) != null) {
+                String[] dados = linha.split(",");
+                System.out.println(dados[2]);
+                SimpleDateFormat dataEntrada = new SimpleDateFormat(dados[2]);
+                SimpleDateFormat dataSaida = new SimpleDateFormat(dados[3]);
+
+                Date dataEnt = dataEntrada.parse(dataEntrada.format(data));
+                Date dataSai = dataSaida.parse(dataFim.format(data));
+                System.out.println(dataEnt);
+
+                if (dataEnt.compareTo(dataInc) >= 0 && dataSai.compareTo(dataF) <= 0
+                        && quarto.getId() == Integer.parseInt(dados[6])) {
+                    status = false;
+                }
+
+            }
+            bufferedReader.close();
+
+            return status;
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao ler o arquivo: " + e.getMessage());
+        }
+        return true;
+    }
+
+    public void buscarQuarto(Quarto quarto, int id) {
+        ArrayList<String> linhas = new ArrayList<String>();
+        try {
+            String caminho = System.getProperty("user.dir");
+
+            FileReader fileReader = new FileReader(caminho + "/" + this.getPath());
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String linha;
+
+            while ((linha = bufferedReader.readLine()) != null) {
+                String[] dados = linha.split(",");
+                if (Integer.parseInt(dados[0]) == id) {
+                    String descricao = dados[1];
+                    int qtdCamaCsal = Integer.parseInt(dados[2]);
+                    int qtdCamaSolteiro = Integer.parseInt(dados[3]);
+                    boolean disponivel = Boolean.parseBoolean(dados[4]);
+                    double preco = Double.parseDouble(dados[5]);
+                    int idHotel = Integer.parseInt(dados[6]);
+                    quarto.setQuarto(id, preco, descricao, disponivel, qtdCamaCsal, qtdCamaSolteiro, idHotel);
+                }
+
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao ler o arquivo: " + e.getMessage());
+        }
+    }
+
+    public void editar(int idQuarto, String conteudo) {
+        ArrayList<String> linhas = new ArrayList<String>();
+        try {
+            String caminho = System.getProperty("user.dir");
+
+            FileReader fileReader = new FileReader(caminho + "/" + this.getPath());
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String linha;
+
+            while ((linha = bufferedReader.readLine()) != null) {
+                String[] quarto = linha.split(",");
+                if (Integer.parseInt(quarto[0]) == idQuarto) {
+                    String novaLinha = conteudo;
+                    linhas.add(novaLinha);
+                } else {
+                    linhas.add(linha);
+                }
+
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao ler o arquivo: " + e.getMessage());
+        }
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(this.getPath()))) {
+            for (String linha : linhas) {
+                bw.write(linha);
+                bw.newLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao criar o arquivo: " + e.getMessage());
+        }
+    }
+
+    public int novoID() {
+        try {
+            int id = 0;
+
+            String caminho = System.getProperty("user.dir");
+            FileReader fileReader = new FileReader(caminho + "/" + this.getPath());
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String linha;
+
+            while ((linha = bufferedReader.readLine()) != null) {
+                String[] quarto = linha.split(",");
+                id = Integer.parseInt(quarto[0]);
+
+            }
+
+            bufferedReader.close();
+            return (id + 1);
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao ler o arquivo: " + e.getMessage());
+        }
+        return 1;
+    }
+
+    public void excluir(int id) {
+        ArrayList<String> linhas = new ArrayList<String>();
+        try {
+            String caminho = System.getProperty("user.dir");
+
+            FileReader fileReader = new FileReader(caminho + "/" + this.getPath());
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String linha;
+
+            while ((linha = bufferedReader.readLine()) != null) {
+                String[] quarto = linha.split(",");
+                if (Integer.parseInt(quarto[0]) != id) {
+                    linhas.add(linha);
+                }
+
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao ler o arquivo: " + e.getMessage());
+        }
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(this.getPath()))) {
+            for (String linha : linhas) {
+                bw.write(linha);
+                bw.newLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao criar o arquivo: " + e.getMessage());
+        }
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
--- src/br/edu/ifg/Hotel.java
+++ src/br/edu/ifg/Hotel.java
@@ -11,21 +11,24 @@ public class Hotel {
 	private Map<Integer, Quarto> quartos = new HashMap<>();
 	private String descricao;
 	private boolean pagamentoAnte;
+	private int idUsuario;
 	private ArquivoHotel bd;
 	private ArquivoQuarto bdQuarto;
-	
+
 	public Hotel() {
 		super();
 		this.bd = new ArquivoHotel("BancoHotel.txt");
 		this.bdQuarto = new ArquivoQuarto("BancoQuarto.txt");
 	}
-	
-	public void cadastrar(String nome, String endereco, String descricao, String cidade, boolean pagamentoAnte) {
-		this.setNome(nome); 
+
+	public void cadastrar(String nome, String endereco, String descricao, String cidade, boolean pagamentoAnte,
+			int idUsuario) {
+		this.setNome(nome);
 		this.setPagamentoAnte(pagamentoAnte);
 		this.setDescricao(descricao);
 		this.setEndereco(endereco);
-		this.setCidade(cidade);
+		this.setCidade(cidade.toLowerCase());
+		this.setIdUsuario(idUsuario);
 		this.setId(this.bd.novoID());
 		this.bd.cadastrar(this.toString());
 	}
@@ -34,106 +37,180 @@ public class Hotel {
 		boolean permissao = usuario.validarPermissao();
 		boolean status = this.validarDados(preco, decricao, qtdCamaCasal, qtdCamaSolterio);
 		Map<Integer, Quarto> quartos = new HashMap<>();
-		
-		if(!permissao) {
+
+		if (!permissao) {
 			System.out.println("Acesso Negado");
 			return;
 		}
-		
-		if(!status) {
+
+		if (!status) {
 			System.out.println("Preencha todos os campos");
 			return;
-		}	
-	
+		}
+
 		Quarto quarto = new Quarto();
 		quarto.cadastrar(preco, decricao, qtdCamaCasal, qtdCamaSolterio, this.getId());
-		
-		quartos.put(this.getQuartos().size()+1, quarto);
-		
+
+		quartos.put(this.getQuartos().size() + 1, quarto);
+
 		System.out.println("Quarto criado");
 	}
-	
+
+	public Map<Integer, Hotel> buscarHoteis(String cidade) {
+		Map<Integer, Hotel> hoteis = new HashMap<>();
+
+		hoteis = this.bd.buscarHoteis(cidade);
+
+		return hoteis;
+	}
+
+	public void buscarHotel(int id) {
+		this.bd.buscarHotel(id, this);
+	}
+
+	public void editar(Usuario usuario, int id, String nome, String endereco, String descricao, String cidade,
+			boolean pagamentoAnte) {
+		boolean permissao = usuario.validarPermissao();
+
+		if (!permissao) {
+			System.out.println("Acesso Negado");
+			return;
+		}
+
+		this.buscarHotel(id);
+		System.out.println(this.getCidade());
+		if (this.cidade == null || this.cidade.isEmpty()) {
+			System.out.println("Hotel não encontrado");
+			return;
+		}
+		this.setHotel(id, nome, endereco, descricao, cidade, pagamentoAnte, usuario.getId());
+		this.bd.editar(id, this.toString());
+		System.out.println("Hotel editado com sucesso!");
+	}
+
+	public void excluir(int id, Usuario usuario) {
+		boolean permissao = usuario.validarPermissao();
+
+		if (!permissao) {
+			System.out.println("Acesso Negado");
+			return;
+		}
+
+		this.buscarHotel(id);
+		System.out.println(this.toString());
+		if (this.cidade == null || this.cidade.isEmpty()) {
+			System.out.println("Hotel não encontrado");
+			return;
+		}
+		this.bd.excluir(id);
+	}
+
+	public void setHotel(int id, String nome, String endereco, String descricao, String cidade, boolean pagamentoAnte,
+			int idUsuario) {
+		this.setNome(nome);
+		this.setPagamentoAnte(pagamentoAnte);
+		this.setDescricao(descricao);
+		this.setEndereco(endereco);
+		this.setCidade(cidade);
+		this.setIdUsuario(idUsuario);
+		this.setId(id);
+	}
+
 	public void buscarQuartos() {
 		this.bdQuarto.buscarTodos(this, this.getId());
 	}
-	
-	public void editarQuarto(Usuario usuario, int idQuarto, double preco, String decricao, int qtdCamaCasal, int qtdCamaSolterio) {
+
+	public void editarQuarto(Usuario usuario, int idQuarto, double preco, String decricao, int qtdCamaCasal,
+			int qtdCamaSolterio) {
 		boolean permissao = usuario.validarPermissao();
 		boolean status = this.validarDados(preco, decricao, qtdCamaCasal, qtdCamaSolterio);
-		
-		if(!permissao) {
+
+		if (!permissao) {
 			System.out.println("Acesso Negado");
 			return;
 		}
-		
-		if(!status) {
+
+		if (!status) {
 			System.out.println("Preencha todos os campos");
 			return;
 		}
-		
-		this.getQuartos().get(idQuarto).editar(idQuarto, preco, decricao, qtdCamaCasal, qtdCamaSolterio, qtdCamaSolterio);		
+
+		this.getQuartos().get(idQuarto).editar(idQuarto, preco, decricao, qtdCamaCasal, qtdCamaSolterio,
+				qtdCamaSolterio);
 	}
-	
+
 	public boolean fazerReserva(int id) {
-	  Quarto quarto = quartos.get(id);
-	    if (quarto != null && quarto.isDisponivel()) {
-	      quarto.reserva();
-	     System.out.println("Reserva do quarto " + id + " feita com sucesso.");
-	      return true;
-	     } else {
-	   	  System.out.println("Nao foi possivel fazer a reserva do quarto " + id + ".");
-	         return false; 
-	        }
-	   }
-		
-	public void liberarQuarto(int id) {
-		 for (Quarto quarto : quartos.values()) {
-		     if (quartos.containsKey(id)) {
-		       quarto.libera();
-		      System.out.println("Quarto " + id + " liberado com sucesso.");
-		        break;
-		      }
-		   System.out.println("Nao foi possivel liberar o quarto " + id + ".");
-		  }
+		Quarto quarto = quartos.get(id);
+		if (quarto != null && quarto.isDisponivel()) {
+			quarto.reserva();
+			System.out.println("Reserva do quarto " + id + " feita com sucesso.");
+			return true;
+		} else {
+			System.out.println("Nao foi possivel fazer a reserva do quarto " + id + ".");
+			return false;
 		}
-		
+	}
+
+	public void liberarQuarto(int id) {
+		for (Quarto quarto : quartos.values()) {
+			if (quartos.containsKey(id)) {
+				quarto.libera();
+				System.out.println("Quarto " + id + " liberado com sucesso.");
+				break;
+			}
+			System.out.println("Nao foi possivel liberar o quarto " + id + ".");
+		}
+	}
+
 	private boolean validarDados(double preco, String decricao, int qtdCamaCasal, int qtdCamaSolterio) {
 		boolean status = true;
-		
-		if(decricao.isEmpty()) status = false;
-		if(preco < 0) status = false;
-		if((qtdCamaCasal+qtdCamaSolterio) <= 1) status = false;
-		
+
+		if (decricao.isEmpty())
+			status = false;
+		if (preco < 0)
+			status = false;
+		if ((qtdCamaCasal + qtdCamaSolterio) <= 1)
+			status = false;
+
 		return status;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEndereco() {
 		return endereco;
 	}
+
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
+
 	public Map<Integer, Quarto> getQuartos() {
 		return quartos;
 	}
+
 	public void setQuartos(Map<Integer, Quarto> quartos) {
 		this.quartos = quartos;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public boolean isPagamentoAnte() {
 		return pagamentoAnte;
 	}
+
 	public void setPagamentoAnte(boolean pagamentoAnte) {
 		this.pagamentoAnte = pagamentoAnte;
 	}
@@ -145,7 +222,7 @@ public class Hotel {
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public String getCidade() {
 		return cidade;
 	}
@@ -154,9 +231,18 @@ public class Hotel {
 		this.cidade = cidade;
 	}
 
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
 	@Override
 	public String toString() {
-		return  id + "," + nome + "," + endereco + "," + descricao + "," + pagamentoAnte;
+		return id + "," + nome + "," + cidade + "," + endereco + "," + descricao + "," + pagamentoAnte + ","
+				+ idUsuario;
 	}
-	
+
 }
